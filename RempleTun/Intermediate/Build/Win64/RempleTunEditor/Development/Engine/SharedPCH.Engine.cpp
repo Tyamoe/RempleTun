440e4b5cf8b068a84d7963c156bad67494e927e8
Chris Thomas <chris.thomas@digipen.edu>
2019-09-17 12:02:41 -0700

    stuff

---
+++ RempleTun/Intermediate/Build/Win64/RempleTunEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/chris.thomas/Desktop/Remple Tun Repo/RempleTun/RempleTun/Intermediate/Build/Win64/RempleTunEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ RempleTun/Intermediate/Build/Win64/UE4Editor/Inc/RempleTun/MyBlueprintFunctionLibrary.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef REMPLETUN_MyBlueprintFunctionLibrary_generated_h
+#error "MyBlueprintFunctionLibrary.generated.h already included, missing '#pragma once' in MyBlueprintFunctionLibrary.h"
+#endif
+#define REMPLETUN_MyBlueprintFunctionLibrary_generated_h
+
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_RPC_WRAPPERS
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUMyBlueprintFunctionLibrary(); \
+	friend struct Z_Construct_UClass_UMyBlueprintFunctionLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UMyBlueprintFunctionLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/RempleTun"), NO_API) \
+	DECLARE_SERIALIZER(UMyBlueprintFunctionLibrary)
+
+
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesUMyBlueprintFunctionLibrary(); \
+	friend struct Z_Construct_UClass_UMyBlueprintFunctionLibrary_Statics; \
+public: \
+	DECLARE_CLASS(UMyBlueprintFunctionLibrary, UBlueprintFunctionLibrary, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/RempleTun"), NO_API) \
+	DECLARE_SERIALIZER(UMyBlueprintFunctionLibrary)
+
+
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UMyBlueprintFunctionLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UMyBlueprintFunctionLibrary) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UMyBlueprintFunctionLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UMyBlueprintFunctionLibrary); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UMyBlueprintFunctionLibrary(UMyBlueprintFunctionLibrary&&); \
+	NO_API UMyBlueprintFunctionLibrary(const UMyBlueprintFunctionLibrary&); \
+public:
+
+
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UMyBlueprintFunctionLibrary(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UMyBlueprintFunctionLibrary(UMyBlueprintFunctionLibrary&&); \
+	NO_API UMyBlueprintFunctionLibrary(const UMyBlueprintFunctionLibrary&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UMyBlueprintFunctionLibrary); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UMyBlueprintFunctionLibrary); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UMyBlueprintFunctionLibrary)
+
+
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_PRIVATE_PROPERTY_OFFSET
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_12_PROLOG
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_PRIVATE_PROPERTY_OFFSET \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_RPC_WRAPPERS \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_INCLASS \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_PRIVATE_PROPERTY_OFFSET \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_INCLASS_NO_PURE_DECLS \
+	RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> REMPLETUN_API UClass* StaticClass<class UMyBlueprintFunctionLibrary>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RempleTun_Source_RempleTun_MyBlueprintFunctionLibrary_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
